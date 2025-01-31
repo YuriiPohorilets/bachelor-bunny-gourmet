d18e8452da26aeec2b365e5a90e6251c7e0dbd98
yuriipohorilets <pohorilets.y@gmail.com>
2025-01-31 20:53:24 +0200

    fixed. fonts

--- src/components/layout/RootLayout/RootLayout.tsx
+++ src/components/layout/RootLayout/RootLayout.tsx
@@ -1,9 +1,46 @@
-import { AnimatePresence, motion } from 'motion/react';
+import localFont from 'next/font/local';
+import { Cormorant_Unicase } from 'next/font/google';
+
 import { usePathname } from 'next/navigation';
+import { AnimatePresence, motion } from 'motion/react';
 
 import { Header, SmoothScroll } from '@/components/layout';
 
 interface IProps extends React.PropsWithChildren {}
+
+export const gothamPro = localFont({
+  src: [
+    { path: '../../../fonts/GothamPro/GothamProLight.woff', weight: '300', style: 'normal' },
+    { path: '../../../fonts/GothamPro/GothamProRegular.woff', weight: '400', style: 'normal' },
+  ],
+  variable: '--font-gotham-pro',
+});
+
+export const centuryGothic = localFont({
+  src: [
+    { path: '../../../fonts/CenturyGothic/CenturyGothic.woff', weight: '400', style: 'normal' },
+    { path: '../../../fonts/CenturyGothic/CenturyGothicBold.woff', weight: '500', style: 'normal' },
+  ],
+  variable: '--font-century-gothic',
+});
+
+export const adobeGaramond = localFont({
+  src: [
+    {
+      path: '../../../fonts/AdobeGaramondPro/AdobeGaramondProRegular.woff',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-adobe-garamond',
+});
+
+export const cormorantUnicase = Cormorant_Unicase({
+  subsets: ['latin'],
+  variable: '--font-cormorant-unicase',
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
 
 export const RootLayout: React.FC<IProps> = ({ children }) => {
   const pathname = usePathname();
@@ -15,7 +52,9 @@ export const RootLayout: React.FC<IProps> = ({ children }) => {
   };
 
   return (
-    <>
+    <div
+      className={`${gothamPro.variable} ${centuryGothic.variable} ${adobeGaramond.variable} ${cormorantUnicase.variable}`}
+    >
       <SmoothScroll>
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
@@ -27,10 +66,11 @@ export const RootLayout: React.FC<IProps> = ({ children }) => {
             key={pathname}
           >
             <Header />
+
             <motion.main>{children}</motion.main>
           </motion.div>
         </AnimatePresence>
       </SmoothScroll>
-    </>
+    </div>
   );
 };
