7cf04fdd58fb8ff1a7249e6ce9ef559ed70b5fff
SayGoodnight <pohorilets.y@gmail.com>
2025-01-31 19:48:03 +0200

    Delete dist

--- dist/_next/static/chunks/pages/404-46f31dfc18dc4572.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{9305:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(8231)}])},8231:function(n,u,_){"use strict";_.r(u);var e=_(5893);_(7294),u.default=()=>(0,e.jsx)("div",{children:"Page404"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9305)}),_N_E=n.O()}]);
